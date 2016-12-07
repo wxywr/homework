3d235de0ab78aa0e5053df48f11ff9cc36cebb7e
wxywr <1261046195@qq.com>
2016-12-07 23:04:51 +0800

    update

--- jquery/shop/js/shop.js
+++ jquery/shop/js/shop.js
@@ -1,7 +1,55 @@
+//数量增加
+$('.p').click(numP);
+function numP(){
+	//console.log(123);
+	var num = Number($(this).prev('input').val());
+	num++;
+	$(this).prev('input').val(num);
+	getxj($(this),num);
+}
+
+//数量减少
+$('.s').click(numS);
+function numS(){
+	var num = Number($(this).next('input').val());
+	if(num<=1){
+		return;
+	}
+	num--;
+	$(this).next('input').val(num);
+	getxj($(this),num);
+}
+
+//小计功能
+function getxj(obj,num){
+	var xjNum = Number(obj.parents('.sl').prev().html())*num;
+	obj.parent().parent().next().html(xjNum.toFixed(1));
+	gethj();
+}
+
+//合计功能
+gethj();
+function gethj(){
+	var hjNum = 0;
+	var xjArr = $('.content .xj');
+	for(var i=0;i<xjArr.length;i++){
+		hjNum+=Number(xjArr.eq(i).html())
+	}
+	$('strong').html(hjNum.toFixed(1));
+}
+
+//删除一行
+$('.del').click(delRow);
+function delRow(){
+	$(this).parent().parent().remove();
+	gethj();
+}
+
+//增加一行
 $('.addRow').click(function(){
 	$('.content').append('<ul class="sp-list"><li class="sp"><img src="img/4.jpg"><span>Casio/卡西欧 EX-TR350</span></li><li class="dj">640.6</li><li class="sl"><div><span class="s">-</span><input type="text" value="1" readonly="readonly"><span class="p">+</span></div></li><li class="xj redColor">640.6</li><li class="cz"><span class="del">删除</span></li></ul>');
+	$('.del').unbind('click').click(delRow);
+	$('.s').unbind('click').click(numS);
+	$('.p').unbind('click').click(numP);
+	gethj();
 });
-
-$('.del').click(function(){
-	$('.sp-list:first').remove();
-})
--- shoping/js/shop.js
+++
@@ -1,6 +0,0 @@
-function addRow(){
-	var footer1=document.getElementsByClassName("footer")[0];
-	var div1=document.createElement("div");
-	footer1.parentNode.insertBefore(div1,footer1);
-	div1.innerHTML='<div class="five"><ul><li class="shop"  class="shop-1"><img src="images/5.jpg"><p>Fujifilm/富士 instaxmini 25</p></li><li li class="price" class="price-1"><p>640.6</p></li><li class="numb"><input type="button" value="-"><input type="text" value="1" class="number"><input type="button" value="+"></li><li class="alls"><p>640.6</p></li><li class="dele"><p>删除</p></li></ul></div>'
-}
